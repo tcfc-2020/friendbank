afae69fdc37db13dbe264f43c40fa6414dac890a
Andrew Kaier <akaier@Andrews-MacBook-Pro.local>
2020-09-24 16:34:37 -0400

    logging

--- src/api/db/getCampaignForDomain.js
+++ src/api/db/getCampaignForDomain.js
@@ -4,11 +4,13 @@ module.exports = async function getCampaignForDomain(db, domain) {
   console.log('Attempting to get campaigns');
   try {
     const campaigns = db.collection('campaigns');
+    console.log(db);
+    console.log(domain);
 
     const campaign = await campaigns.findOne({ domains: domain });
     
     console.log('Found Campaign.');
-    console.log(campaign.toString());
+    console.log(campaign);
     console.log('End Campaign');
 
     return campaign;
